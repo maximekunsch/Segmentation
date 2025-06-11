362f4b4cd1c2500c5db629c29df4db2f466a7832
maximekunsch <maximekunsch.ai@gmail.com>
2025-06-11 14:47:58 +1000

    Update Segmentation.py

--- Segmentation.py
+++ Segmentation.py
@@ -6,47 +6,43 @@ import cv2
 from matplotlib import pyplot as plt
 
 def my_segmentation(img, img_mask):
-    # Inversion des couleurs et mask
+    # Inversion of colors and mask
     img_pretreat = (255 * img_mask & (np.invert(img))).astype(np.uint8)
 
-    # Multi-scales top-hat transformation
-    scales = [3,5,7]  # Différentes échelles pour le top-hat
-    # Initialisation de l'image de sortie
-    # pour le top-hat multi-échelle
+    # Multi-scale top-hat transformation
+    scales = [3, 5, 7]  # Different scales for the top-hat
+    # Initialization of the output image for multi-scale top-hat
     img_tophat_multi = np.zeros_like(img_pretreat)
 
-    # Application du top-hat blanc pour chaque échelle
-    # et combinaison des résultats
+    # Applying white top-hat for each scale and combining the results
     for scale in scales:
         img_tophat_multi = np.maximum(img_tophat_multi, white_tophat(img_pretreat, disk(scale)))
 
-    # Seuil de l'intensité
-    seuil = 15
-    img_binaire = img_tophat_multi > seuil
+    # Intensity threshold
+    threshold = 15
+    img_binary = img_tophat_multi > threshold
 
-    # Conversion pour le traitement des composantes connectées
-    # Conversion de l'image binaire en uint8 pour cv2
-    seuil_uint8 = (img_binaire * 255).astype(np.uint8)
+    # Conversion for connected component processing
+    # Conversion of the binary image to uint8 for cv2
+    threshold_uint8 = (img_binary * 255).astype(np.uint8)
 
-    # Trouver les composantes connectées de l'image binaire
-    # et filtrer les petites composantes
-    # Utilisation de cv2 pour les composantes connectées
-    num_comp, comps = cv2.connectedComponents(seuil_uint8)
+    # Find connected components of the binary image and filter small components
+    # Use cv2 for connected components
+    num_comp, comps = cv2.connectedComponents(threshold_uint8)
 
-    # Filtrer les composantes par taille
-    img_connexe = np.zeros_like(img_binaire)
+    # Filter components by size
+    img_connexe = np.zeros_like(img_binary)
     for comp in range(1, num_comp):
         component = (comps == comp)
         component_size = np.sum(component)
-        if component_size > 25:  # Taille minimale pour les capillaires
+        if component_size > 25:  # Minimum size for capillaries
             img_connexe |= component
 
-    # Masque circulaire pour se concentrer sur la région d'intérêt
+    # Circular mask to focus on the region of interest
     mask = np.zeros_like(img_connexe, dtype=np.uint8)
     mask = cv2.circle(mask, (256, 256), 253, 1, -1)
 
-    # Application du masque circulaire
-    # pour ne garder que les pixels dans le disque inscrit
+    # Applying the circular mask to keep only the pixels within the inscribed disk
     img_out = img_connexe & mask
 
     return img_out
@@ -62,38 +58,39 @@ def evaluate(img_out, img_GT):
     RECALL = TP / (TP + FN)
     return ACCU, RECALL, img_out_skel, GT_skel
 
-#Ouvrir l'image originale en niveau de gris
-img =  np.asarray(Image.open('./images_IOSTAR/star02_OSC.jpg')).astype(np.uint8)
+# Open the original image in grayscale
+img = np.asarray(Image.open('./images_IOSTAR/star02_OSC.jpg')).astype(np.uint8)
 print(img.shape)
 
 nrows, ncols = img.shape
 row, col = np.ogrid[:nrows, :ncols]
-#On ne considere que les pixels dans le disque inscrit 
+
+# Consider only the pixels within the inscribed disk
 img_mask = (np.ones(img.shape)).astype(np.bool_)
 invalid_pixels = ((row - nrows/2)**2 + (col - ncols/2)**2 > (nrows / 2)**2)
 img_mask[invalid_pixels] = 0
 
-img_out = my_segmentation(img,img_mask)
+img_out = my_segmentation(img, img_mask)
 
-#Ouvrir l'image Verite Terrain en booleen
-img_GT =  np.asarray(Image.open('./images_IOSTAR/GT_02.png')).astype(np.uint8)
+# Open the Ground Truth image as boolean
+img_GT = np.asarray(Image.open('./images_IOSTAR/GT_02.png')).astype(np.uint8)
 
 ACCU, RECALL, img_out_skel, GT_skel = evaluate(img_out, img_GT)
-print('Accuracy =', ACCU,', Recall =', RECALL)
+print('Accuracy =', ACCU, ', Recall =', RECALL)
 
 plt.subplot(231)
-plt.imshow(img,cmap = 'gray')
-plt.title('Image Originale')
+plt.imshow(img, cmap='gray')
+plt.title('Original Image')
 plt.subplot(232)
-plt.imshow(img_out,cmap='gray')
+plt.imshow(img_out, cmap='gray')
 plt.title('Segmentation')
 plt.subplot(233)
-plt.imshow(img_out_skel,cmap='gray')
-plt.title('Segmentation squelette')
+plt.imshow(img_out_skel, cmap='gray')
+plt.title('Segmentation Skeleton')
 plt.subplot(235)
-plt.imshow(img_GT,cmap='gray')
-plt.title('Verite Terrain')
+plt.imshow(img_GT, cmap='gray')
+plt.title('Ground Truth')
 plt.subplot(236)
-plt.imshow(GT_skel,cmap='gray')
-plt.title('Verite Terrain Squelette')
+plt.imshow(GT_skel, cmap='gray')
+plt.title('Ground Truth Skeleton')
 plt.show()
